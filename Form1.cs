920a109aa8cc552194373e26e8db1d70bc5cbafd
Felipe de Senco <55833207+Dijoig@users.noreply.github.com>
2023-01-12 23:43:25 +0000

    Added iniparser Added logic to create a full copy of ini file into new.ini

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,6 @@
+using static System.Windows.Forms.LinkLabel;
+using System;
+
 namespace Ut3CustomCrosshairs
 {
     public partial class Form1 : Form
@@ -23,7 +26,7 @@ namespace Ut3CustomCrosshairs
             {
                 File_Incorrect_Setup();
             }
-        }       
+        }
 
         private void selectFileButton_click(object sender, EventArgs e)
         {
@@ -34,15 +37,24 @@ namespace Ut3CustomCrosshairs
         {
             this.label1.Text = this.openFileDialog.FileName;
             this.selectFileButton.Dispose();
+            IniParser weaponIni = new IniParser(this.openFileDialog.FileName);
+            weaponIni.GetSetting("UTGame.UTWeap_Enforcer", "Priority");
+            if (weaponIni is not null)
+            {
+                //REMEMBER THE TARGETaDHESIONMAXDISTANCE IS DUPLICATED ON UTWEAPON.INI SO THAT IS NOT COPIED TWICE ON THE NEW FILE
+                weaponIni.SaveSettings("new.ini");                
+            }
         }
+
         private void File_Incorrect_Setup()
         {
-            this.label1.Text = "The UTWeapon.ini file path is not working. Please use the button to try again:";
+            this.label1.Text =
+                "The UTWeapon.ini file path is not working. Please use the button to try again:";
         }
 
         private bool File_Path_isCorrect(string filePath)
         {
-            return filePath.Contains("UTWeapon.ini");           
+            return filePath.Contains("UTWeapon.ini");
         }
     }
 }
